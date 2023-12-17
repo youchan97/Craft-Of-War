0382e956edd622cb766e8251f204af2cb3e06528
BlackKoneko <hdicmsr@naver.com>
2023-12-18 00:27:14 +0900

    상점 구매 기능 버그 수정중

--- Assets/Script/GameStruct/Inven/InventoryManager.cs
+++ Assets/Script/GameStruct/Inven/InventoryManager.cs
@@ -9,31 +9,64 @@ public class InventoryManager : SingleTon<InventoryManager>
     public Inventory inven;
     public InventoryItem waitForSelling = null;
 
+
     public void AddItem(GameObject item)
     {
+        //for (int i = 0; i < inven.slots.Length; i++)
+        //{
+        //    if (inven.slots[i].item == null)
+        //    {
+        //        if (inven.curItemCount == inven.maxSlotCount) return;
+        //        inven.slots[i].item = Instantiate(item, inven.slots[i].transform).GetComponent<InventoryItem>();
+        //        inven.curItemCount++;
+        //        break;
+        //    }
+        //    else
+        //    {
+        //        if (inven.slots[i].item.Stackable 
+        //            && inven.slots[i].item.ID == item.GetComponent<InventoryItem>().itemData.ID)
+        //        {
+        //                inven.slots[i].item.Count++;
+        //                inven.slots[i].item.RefreshCount();
 
-        for (int i = 0; i < inven.slots.Length; i++)
+        //            break;
+        //        }
+        //    }
+        //}
+        if (inven.curItemCount == inven.maxSlotCount)   //아이템 슬룻이 꽉차 있을때
+            return;
+        if (inven.curItemCount == 0)                    //아이템 슬룻에 아이템이 없을때
         {
-            if (inven.slots[i].item == null)
+            inven.slots[0].item = Instantiate(item, inven.slots[0].transform).GetComponent<InventoryItem>();
+            inven.curItemCount++;
+            return;
+        }
+        else                                            //아이템 슬룻에 아이템이 있을때
+        {
+            for (int k = 0; k < inven.slots.Length; k++)//아이템 슬룻에 스택이 가능할때
             {
-                if (inven.curItemCount == inven.maxSlotCount) return;
-                inven.slots[i].item = Instantiate(item, inven.slots[i].transform).GetComponent<InventoryItem>();
-                inven.curItemCount++;
-                break;
-            }
-            else
-            {
-                if (inven.slots[i].item.Stackable 
-                    && inven.slots[i].item.ID == item.GetComponent<InventoryItem>().itemData.ID)
+                if (inven.slots[k].item != null)
                 {
-                        inven.slots[i].item.Count++;
-                        inven.slots[i].item.RefreshCount();
-
-                    break;
+                    if (inven.slots[k].item.Stackable && inven.slots[k].item.ID == item.GetComponent<InventoryItem>().itemData.ID)
+                    {   //같은 아이템이 있을경우
+                        inven.slots[k].item.Count++;
+                        inven.slots[k].item.RefreshCount();
+                        return;
+                    }
                 }
             }
+            for (int s = 0; s < inven.slots.Length; s++)
+            {           //같은 아이템이 없을경우
+                if (inven.slots[s].item == null)
+                {
+                    inven.slots[s].item = Instantiate(item, inven.slots[s].transform).GetComponent<InventoryItem>();
+                    inven.curItemCount++;
+                    return;
+                }
+            }        
         }
     }
+
 
 
 }
