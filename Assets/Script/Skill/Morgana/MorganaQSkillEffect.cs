ec19cd96961dd820020d26348bd2d957d42cfd53
Hoemins <shghlals1717@gmail.com>
2023-12-20 18:03:14 +0900

    영웅 애니메이터 생성(수정중), 모르가나 Q 스킬 수정중

--- Assets/Script/Skill/Morgana/MorganaQSkillEffect.cs
+++ Assets/Script/Skill/Morgana/MorganaQSkillEffect.cs
@@ -4,5 +4,27 @@ using UnityEngine;
 
 public class MorganaQSkillEffect : MonoBehaviour
 {
-    
+    [SerializeField] float moveSpeed;
+    [SerializeField] private Vector3 direction;
+    [SerializeField] GameObject hitEffect;
+    Rigidbody rb;
+    public Morgana owner;
+
+    public Vector3 Direction
+    {
+        get => direction;
+        set
+        {
+            direction = value.normalized;
+        }
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        owner = (Morgana)GameManager.Instance.PlayerHero;
+    }
+
+
+
 }
